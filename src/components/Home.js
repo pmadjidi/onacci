a221400e6803b4ad51537cde5554196c8bba43fc
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-06-12 13:06:38 +0200

    yet an other commit.....

--- src/components/Home.js
+++ src/components/Home.js
@@ -547,7 +547,7 @@ processEmoji(emoji) {
         <div className="HomeInfo">
           <img  src={"/avatar/" + this.props.team + "/" + this.props.username + ".png"} className ="statusBarImage" ref={img => this.img = img} onError={(e)=>{e.target.src='/images/onacci.png'}} />
           {this.CL(this.props.username)}
-          <div style = {{float: "right"}}>{this.connection}</div>
+          <div style = {{float: "right"}}>{connection}</div>
         </div>
         </div>
         <Links />
