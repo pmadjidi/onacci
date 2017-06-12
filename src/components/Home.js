e7b87b9c2fd55c5fc3a286ed4dd9375b526463d6
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-06-12 10:49:24 +0200

    yet an other commit.....

--- src/components/Home.js
+++ src/components/Home.js
@@ -539,10 +539,10 @@ processEmoji(emoji) {
         <div className="HomeInfo">
           <img  src={"/avatar" + this.props.team + "/" + this.props.username + ".png"} className ="onlineImage" ref={img => this.img = img} onError={(e)=>{e.target.src='/images/onacci.png'}} />
           {this.CL(this.props.username)}
-          {if (this.state.commState)
+          {() = > {if (this.state.commState)
               <Emoji emoji={"arrows_clockwise"} size={32}/>
           else
-          <Emoji emoji={"x"} size={32}/>}
+          <Emoji emoji={"x"} size={32}/>}}
         </div>
         </div>
         <Links />
