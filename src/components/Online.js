8ad43e61e4116cbf7cef2d5bd025811bfb6cb8e9
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-06-12 08:06:10 +0200

    yet an other commit.....

--- src/components/Online.js
+++ src/components/Online.js
@@ -22,7 +22,9 @@ CL(string) {
 
 createOnline(onlineUser,index) {
       if (onlineUser)
-        return <li key={index} className={onlineUser.status + " onlineLi"}  onClick={()=>this.props.action(onlineUser)}><Emoji emoji={"bust_in_silhouette"} size={16}/>{" " + this.CL(onlineUser.name)}<span id={index} className="HomeInfo">{onlineUser.notify > 0 ? onlineUser.notify:null}</span></li>
+        return <li key={index} className={onlineUser.status + " onlineLi"}  onClick={()=>this.props.action(onlineUser)}>
+          <img src={avatar} alt="Avatar" className ="w3-left  w3-margin-right w3-img" ref={img => this.img = img} onError={(e)=>{e.target.src='/images/onacci.png'}
+          {" " + this.CL(onlineUser.name)}<span id={index} className="HomeInfo">{onlineUser.notify > 0 ? onlineUser.notify:null}</span></li>
   }
 
 
