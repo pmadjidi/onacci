3e84186e11406252f7c3cdb52212f3f51b799f89
Payam Madjidi <payam@Payams-MacBook-Pro.local>
2017-06-14 12:28:45 +0200

    yet an other commit.....

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -20,7 +20,7 @@ createChannel(team,index) {
       let avatar = "/avatar/team/" + team + "/" + team  + ".png"
       return <li key={index} onClick={()=>this.props.action(team)}>
         <div>
-        <img  className ="onlineImage" src={avatar} ref={img => this.img = img} onError={(e)=>{e.target.src='/images/onacci.png'}} />
+        <img  className ="onlineImage"  data-tooltip="Current Team...." src={avatar} ref={img => this.img = img} onError={(e)=>{e.target.src='/images/onacci.png'}} />
          <span class="caption">{this.CL(team)}</span>
       </div>
       </li>
